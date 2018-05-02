91f8c961096dae92d885b4db1868f196f0c1ae43
microshak <roshak@hotmail.com>
2018-05-02 13:19:58 -0700

    Working example

--- kafkasender.py
+++ kafkasender.py
@@ -4,8 +4,8 @@ import json
 MessagesToSend = 0
 
 if __name__ == '__main__':  
-    #producer = KafkaProducer(bootstrap_servers='wn0-microk.l1nji5hfpjbe5g4bvryy2aon2a.gx.internal.cloudapp.net:9092')
-    producer = KafkaProducer(bootstrap_servers='iotlab-cluster7:9092')
+    
+    producer = KafkaProducer(bootstrap_servers='[Your Public Ip Address]:9092')
 
     buildData = BuildData()
     template = buildData.loadTemplate("template.json")
